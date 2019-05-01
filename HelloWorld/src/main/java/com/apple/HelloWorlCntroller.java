48b74195d1fa11715a2eb96cc542988944e6e0b3
Priyanka Ronge <rongepriyanka11@gmail.com>
2019-05-01 13:10:55 +0530

    Committing HelloSpring code to remote repository

---
+++ HelloWorld/src/main/java/com/apple/HelloWorlCntroller.java
@@ -0,0 +1,21 @@
+package com.apple;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorlCntroller {
+ String message = "welcome to spring mvc";
+ 
+ @RequestMapping("/hello")
+ public ModelAndView showmessage (@RequestParam(value= "name" ,required=false) String name) {
+	 ModelAndView mv = new ModelAndView("helloworld");
+	 mv.addObject("message", message);
+	 mv.addObject("name", name);
+	 return mv;
+ }
+}
